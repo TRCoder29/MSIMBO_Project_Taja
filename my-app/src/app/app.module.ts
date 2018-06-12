ba239c2e4aafb2bf5ea97de94f4bc27ec545fd8b
TSN Student <student@ulem.org>
2018-06-12 16:04:29 -0400

    Tuesday June 12 Afternoon Updates

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,11 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/Business/home/home.component';
+import { AboutComponent } from './components/Business/about/about.component';
+import { BlogComponent } from './components/Business/blog/blog.component';
+import { ContactComponent } from './components/Business/contact/contact.component';
+import { FaqComponent } from './components/Business/faq/faq.component';
+import { AdminDashboardComponent } from './components/Business/admin-dashboard/admin-dashboard.component';
+import { LoginComponent } from './components/User/login/login.component';
+import { RegisterComponent } from './components/User/register/register.component';
+import { ProfileComponent } from './components/User/profile/profile.component';
+import { ItineraryComponent } from './components/business/itinerary/itinerary.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    FaqComponent,
+    AdminDashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ItineraryComponent
   ],
   imports: [
     BrowserModule
